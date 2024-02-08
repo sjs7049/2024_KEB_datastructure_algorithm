40bd6525e8028fdd3c775d2dc8a3191057a52af5
sjs7049 <sjs7049@gmail.com>
2024-02-08 11:24:33 +0900

    v0.5 ocp

--- mymath.py
+++ mymath.py
@@ -1,26 +1,36 @@
 import time
 
-def factorial(number) -> int:
-    '''
-    factorial by repitition
-    :param number: number (int)
-    :return: factorial result
-    '''
-    result = 1
-    for i in range(1, number):
-        result *= i
-    return result
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'time elapsed: {end - start}')
+        return result
+    return wrapper
 
 # def factorial(number) -> int:
 #     '''
-#     factorial by recursion
+#     factorial by repetition
 #     :param number: number (int)
 #     :return: factorial result
 #     '''
-#     if number == 0 or number == 1:
-#         return 1
-#     return number * factorial(number-1)
+#     result = 1
+#     for i in range(1, number):
+#         result *= i
+#     return result
 
+def factorial(number) -> int:
+    '''
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial result
+    '''
+    if number == 0 or number == 1:
+        return 1
+    return number * factorial(number-1)
+
+@timer
 def nCr(n, r) -> int:
     '''
     combination function
@@ -28,9 +38,6 @@ def nCr(n, r) -> int:
     :param r: 선택 개수
     :return: nCr 값
     '''
-    start = time.time()
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
-    end = time.time()
-    print(f'소요 시간: {end - start}')
     return int(numerator / denominator)
