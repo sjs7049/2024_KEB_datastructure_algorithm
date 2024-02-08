49e27e6222a8f7f5c8159e2869153db959296ea1
sjs7049 <sjs7049@gmail.com>
2024-02-08 13:50:04 +0900

    v0.7 guess number

--- day 1.py
+++ day 1.py
@@ -1,5 +1,19 @@
-import mymath
+import random
 
-if __name__ == "__main__":
-    n,r = map(int, input("Input n, r : ").split())
-    print(f'{n}C{r} = {mymath.nCr(n, r)}')
+ans = random.randint(1,100)
+chance = 7
+
+while True:
+    guess = int(input("Input the guess number : "))
+    chance -= 1
+    if guess == ans:
+        print(f"That's correct. Random number is {ans}!!")
+        break
+    elif guess < ans:
+        print(f"The number is greater than {guess}.")
+    else:
+        print(f"The number is lower than {guess}.")
+
+    if chance == 0:
+        print("You've exhausted our chances of guessing. Random number is {ans}!!")
+        break
