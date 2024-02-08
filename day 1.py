1c8ecf99e831d1756755167e58ac526c181f78ca
sjs7049 <sjs7049@gmail.com>
2024-02-08 14:07:26 +0900

    v0.7 guess number

--- day 1.py
+++ day 1.py
@@ -3,7 +3,7 @@ import random
 ans = random.randint(1,100)
 chance = 7
 
-while True:
+while chance != 0:
     guess = int(input("Input the guess number : "))
     chance -= 1
     if guess == ans:
@@ -13,7 +13,5 @@ while True:
         print(f"The number is greater than {guess}.")
     else:
         print(f"The number is lower than {guess}.")
-
-    if chance == 0:
-        print("You've exhausted our chances of guessing. Random number is {ans}!!")
-        break
+else:
+    print("You've exhausted our chances of guessing. Random number is {ans}!!")
