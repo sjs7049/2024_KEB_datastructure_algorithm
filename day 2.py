548a92fdd74db18dbd4f86c6108c92a75c35e2fa
sjs7049 <sjs7049@gmail.com>
2024-02-13 14:32:16 +0900

    v0.9 stack bracket

---
+++ day 2.py
@@ -0,0 +1,22 @@
+def check_bracket(expr: str) -> bool:
+    '''
+    Check bracket in expression.
+    :param expr: str
+    :return: bool
+    '''
+    stack = []
+    table = {')': '(', ']': '[', '}': '{', '>': '<'}
+    for char in expr:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+            return False
+        else:
+            pass
+    return len(stack) == 0
+
+
+if __name__ == "__main__" :
+    expression = input("Input expression : ")
+
+    print(check_bracket(expression))
