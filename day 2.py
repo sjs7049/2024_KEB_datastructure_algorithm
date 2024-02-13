fcbca3e3d781f934542fb79f2cf2e6e49b8d2894
sjs7049 <sjs7049@gmail.com>
2024-02-13 20:49:17 +0900

    self study 4-1

--- day 2.py
+++ day 2.py
@@ -1,42 +1,65 @@
-def print_poly(f_x):
-    term = len(f_x) - 1
-    poly_str = "f(x) = "
+class Node:
+	def __init__ (self):
+		self.data = None
+		self.link = None
 
-    for i in range(len(fx)):
-        coef = f_x[i]
+def print_nodes(start):
+	current = start
+	if current is None:
+		return
+	print(current.data, end=' ')
+	while current.link is not None:
+		current = current.link
+		print(current.data, end=' ')
+	print()
 
-        if coef == 0:
-            term -= 1
-            continue
-        if coef >= 0 and i != 0:
-            poly_str += "+"
-        poly_str += str(coef) + "x^" + str(term) + " "
-        term -= 1
+def delete_node(delete_data):
+	global head, current, pre
 
-    return poly_str
+	if head.data == delete_data:
+		current = head
+		head = head.link
+		print("# 첫 노드가 삭제됨 #")
+		del current
+		return
 
+	current = head
+	while current.link is not None:
+		pre = current
+		current = current.link
+		if current.data == delete_data:
+			pre.link = current.link
+			print("# 중간 노드가 삭제됨 #")
+			del current
+			return
+	else:
+		print("# 삭제된 노드가 없음 #")
 
-def calc_poly(x_val, f_x):
-    ret_value = 0
-    term = len(f_x) - 1
-
-    for i in range(len(fx)):
-        coef = f_x[i]
-        ret_value += coef * pow(x_value, term)
-        term -= 1
-
-    return ret_value
-
-
-fx = [7, -4, 0, 5]
+head, current, pre = None, None, None
+data_array = ["다현", "정연", "쯔위", "사나", "지효"]
 
 if __name__ == "__main__":
-    f_str = print_poly(fx)
-    print(f_str)
+	node = Node()
+	node.data = data_array[0]
+	head = node
 
-    x_value = int(input("X 값 : "))
+	for data in data_array[1:]:
+		pre = node
+		node = Node()
+		node.data = data
+		pre.link = node
 
-    pxValue = calc_poly(x_value, fx)
-    print(pxValue)
 
+	print_nodes(head)
 
+	delete_node("다현")
+	print_nodes(head)
+
+	delete_node("쯔위")
+	print_nodes(head)
+
+	delete_node("사나")
+	print_nodes(head)
+
+	delete_node("재남")
+	print_nodes(head)
