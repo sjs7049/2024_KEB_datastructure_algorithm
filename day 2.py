462249becf564c13f99edf97d4cefe260659fc3e
sjs7049 <sjs7049@gmail.com>
2024-02-13 20:57:11 +0900

    self study 5-1

--- day 2.py
+++ day 2.py
@@ -3,49 +3,56 @@ class Node:
 		self.data = None
 		self.link = None
 
-def print_nodes(start) :
+def print_nodes(start):
 	current = start
-	if current is None:
+	if current.link is None :
 		return
 	print(current.data, end=' ')
-	while current.link is not None:
+	while current.link != start:
 		current = current.link
 		print(current.data, end=' ')
 	print()
 
-def make_simple_linked_list(namePhone):
+def find_node(find_data):
 	global head, current, pre
-	print_nodes(head)
-
-	node = Node()
-	node.data = namePhone
-	if head is None:
-		head = node
-		return
-
-	if head.data[1] > namePhone[1]:
-		node.link = head
-		head = node
-		return
 
 	current = head
-	while current.link is not None :
-		pre = current
+	if current.data == find_data:
+		print("# 첫 노드에서 찾음 #")
+		return current
+	while current.link != head:
 		current = current.link
-		if current.data[1] > namePhone[1]:
-			pre.link = node
-			node.link = current
-			return
-
-	current.link = node
+		if current.data == find_data:
+			print("# 중간 노드에서 찾음 #")
+			return current
+	else:
+		print("# 찾는 노드가 없음 #")
+	return Node()
 
 
 head, current, pre = None, None, None
-data_array = [["지민", "180"], ["정국", "177"], ["뷔", "183"], ["슈가", "175"], ["진", "179"]]
+data_array = ["다현", "정연", "쯔위", "사나", "지효"]
 
 if __name__ == "__main__":
+	node = Node()
+	node.data = data_array[0]
+	head = node
+	node.link = head
 
-	for data in data_array:
-		make_simple_linked_list(data)
+	for data in data_array[1:]:
+		pre = node
+		node = Node()
+		node.data = data
+		pre.link = node
+		node.link = head
 
 	print_nodes(head)
+
+	fNode = find_node("다현")
+	print(fNode.data)
+
+	fNode = find_node("쯔위")
+	print(fNode.data)
+
+	fNode = find_node("재남")
+	print(fNode.data)
