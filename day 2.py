d7be11e1839fa018fdf0936fb2656302132a27d7
sjs7049 <sjs7049@gmail.com>
2024-02-13 21:04:32 +0900

    self study 5-2

--- day 2.py
+++ day 2.py
@@ -1,58 +1,86 @@
 class Node:
-	def __init__ (self):
-		self.data = None
-		self.link = None
+    def __init__ (self):
+        self.data = None
+        self.link = None
+
+def process_minus(data_array):
+    global head, current
+    count_odd = 0
+    count_even = 0
+    for i in data_array:
+        if i % 2 == 0:
+            count_even += 1
+        else:
+            count_odd += 1
+
+    if count_odd > count_even:
+        res = 1
+    else:
+        res = 0
+
+    current = head
+    while True:
+        if current.data % 2 == res:
+            current.data *= -1
+        if current.link == head:
+            break
+        current = current.link
+
+def process_toggle(data_array):
+    global head, current
+    cnt_positive = 0
+    cnt_negative = 0
+    cnt_zero = 0
+
+    for i in data_array:
+        if i > 0:
+            cnt_positive += 1
+        elif i < 0:
+            cnt_negative += 1
+        else:
+            cnt_zero += 1
+
+    print(f"양수 : {cnt_positive}, 음수 : {cnt_negative}, 0 : {cnt_zero}")
+
+    current = head
+    while True:
+        current.data *= -1
+        if current.link == head:
+            break
+        current = current.link
 
 def print_nodes(start):
-	current = start
-	if current.link is None :
-		return
-	print(current.data, end=' ')
-	while current.link != start:
-		current = current.link
-		print(current.data, end=' ')
-	print()
-
-def find_node(find_data):
-	global head, current, pre
-
-	current = head
-	if current.data == find_data:
-		print("# 첫 노드에서 찾음 #")
-		return current
-	while current.link != head:
-		current = current.link
-		if current.data == find_data:
-			print("# 중간 노드에서 찾음 #")
-			return current
-	else:
-		print("# 찾는 노드가 없음 #")
-	return Node()
+    current = start
+    if current.link is None :
+        return
+    print(current.data, end=' ')
+    while current.link != start:
+        current = current.link
+        print(current.data, end=' ')
+    print()
 
 
+import random
 head, current, pre = None, None, None
-data_array = ["다현", "정연", "쯔위", "사나", "지효"]
+data_array = [random.randint(-100, 100) for _ in range(7)]
 
 if __name__ == "__main__":
-	node = Node()
-	node.data = data_array[0]
-	head = node
-	node.link = head
+    node = Node()
+    node.data = data_array[0]
+    head = node
+    node.link = head
 
-	for data in data_array[1:]:
-		pre = node
-		node = Node()
-		node.data = data
-		pre.link = node
-		node.link = head
+    for data in data_array[1:]:
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
+        node.link = head
 
-	print_nodes(head)
+    print_nodes(head)
+    process_toggle(data_array)
+    print_nodes(head)
 
-	fNode = find_node("다현")
-	print(fNode.data)
 
-	fNode = find_node("쯔위")
-	print(fNode.data)
 
-	fNode = find_node("재남")
-	print(fNode.data)
+
