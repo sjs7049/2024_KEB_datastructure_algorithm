490bce261069ce78b23300245cba2f23b9ab2e7d
sjs7049 <sjs7049@gmail.com>
2024-02-14 14:35:00 +0900

    v1.3 dfs

--- day 3.py
+++ day 3.py
@@ -1,19 +1,21 @@
 graph = [
-    [0, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0],
 ]
 
 def dfs(g, v, visited):
     visited[v] = True
-    print(chr(ord('A')+v), end=' ')
+    print(name_array[v], end=' ')
     for i in range(len(g)):
         if graph[v][i] == True and not visited[i]:
             dfs(g, i, visited)
 
 
-
-visited = [0] * len(graph)
+name_array = ["문별", "솔라", "휘인", "쯔위", "선미", "화사"]
+visited = [0 for _ in range(len(graph))]
+print(f'방문 순서 ---> ', end='')
 dfs(graph, 0, visited)
