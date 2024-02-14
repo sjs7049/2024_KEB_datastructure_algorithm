2eedca2fb1af588203b387573fa7ece4e9369401
sjs7049 <sjs7049@gmail.com>
2024-02-14 22:28:11 +0900

    example 07-01

--- day 2.py
+++ day 2.py
@@ -1,17 +1,36 @@
-sentences = []
+def is_queue_empty():
+    global front, rear
+    return front == rear
 
-def str_reverse(sen):
-    return sen[::-1]
+def en_queue(data):
+    global queue, rear
+    rear += 1
+    queue[rear] = data
 
-for _ in range(4):
-    string = input()
-    sentences.append(string)
+def de_queue():
+    global queue, front, rear
+    if is_queue_empty():
+        return None
+    front += 1
+    data = queue[front]
+    queue[front] = None
+    for i in range(front + 1, rear + 1):
+        queue[i - 1] = queue[i]
+        queue[i] = None
+    front -= 1
+    rear -= 1
+    return data
 
-for i in range(3,-1,-1):
-    rev = str_reverse(sentences[i])
-    print(rev, end='')
-    print()
+queue = ['정국', '뷔', '지민', '진', '슈가']
+front = rear = -1
 
+if __name__ == "__main__":
+    for i in queue:
+        en_queue(i)
+    for _ in range(rear + 1):
+        print("대기 줄 상태 :", queue)
+        out_person = de_queue()
+        print(f"{out_person}님 식당에 들어감")
 
-
-
+    print("대기 줄 상태 :", queue)
+    print("식당 영업 종료")
