30038139cb3cf21e31a2dd1b299081168fdbf6d5
sjs7049 <sjs7049@gmail.com>
2024-02-14 20:38:47 +0900

    ãexample 06-01

--- day 2.py
+++ day 2.py
@@ -1,39 +1,41 @@
 class Node:
     def __init__(self):
         self.data = None
-        self.front_link = None
-        self.back_link = None
+        self.forward_link = None
+        self.reverse_link = None
 
 def print_nodes(start):
     current = start
-    if current.back_link is None:
+    if current.forward_link is None:
         return
-    print("정방향 ---> ", end=' ')
-    print(current.data, end=' ')
-    while current.back_link is not None:
-        current = current.back_link
-        print(current.data, end=' ')
-    print()
+    print("과자 집에 가는 길 : ", end=' ')
+    print(f"{current.data} --->", end=' ')
+    while current.forward_link is not None:
+        current = current.forward_link
+        print(f"{current.data} --->", end=' ')
+    print('과자집')
 
-    print("역방향 ---> ", end=' ')
-    print(current.data, end=' ')
-    while current.front_link is not None:
-        current = current.front_link
-        print(current.data, end=' ')
-    print()
+    print("우리 집에 가는 길 : ", end=' ')
+    print(f"{current.data} --->", end=' ')
+    while current.reverse_link is not None:
+        current = current.reverse_link
+        print(f"{current.data} --->", end=' ')
+    print('우리집')
+
 
 head, current, pre = None, None, None
-name_array = ['다현', '정연', '쯔위', '사나', '지효']
+color_array = ['주황', '초록', '파랑', '보라', '빨강', '노랑']
+
 if __name__ == "__main__":
     node = Node()
-    node.data = name_array[0]
+    node.data = color_array[0]
     head = node
 
-    for name in name_array[1:]:
+    for color in color_array[1:]:
         pre = node
         node = Node()
-        node.data = name
-        pre.back_link = node
-        node.front_link = pre
+        node.data = color
+        pre.forward_link = node
+        node.reverse_link = pre
 
     print_nodes(head)
