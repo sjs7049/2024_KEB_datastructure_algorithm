56d2187e857b326e814945e7e94c96c3f3b46cb2
sjs7049 <sjs7049@gmail.com>
2024-02-15 11:20:35 +0900

    v2.1 fibonacci after gui

--- day 4.py
+++ day 4.py
@@ -16,22 +16,27 @@ def fibo_memoization(number) -> int :
     memo[number] = result
     return result
 
-# create window object
-w = tk.Tk()
-w.title("Fibonacci")
-w.geometry("250x100")
+def process_fibonacci():
+    number = int(en_input_number.get())
+    lbl_display_fibonacci_recursion.config(text=f"fibonacci({number}) = {fibo_memoization(number)}")
 
-# create widget
-lbl_display_fibonacci_recursion = tk.Label(w, text='Fibonacci by memoization')
-en_input_number = tk.Entry(w)
-btn_click = tk.Button(w, text='Click')
+if __name__ == "__main__":
+    w = tk.Tk() # create window object
+    w.title("Fibonacci")
+    w.geometry("250x100")
 
-# layout
-lbl_display_fibonacci_recursion.pack()
-en_input_number.pack(fill="x")
-btn_click.pack(fill="x")
+    # create widget
+    lbl_display_fibonacci_recursion = tk.Label(w, text='Fibonacci by memoization')
+    en_input_number = tk.Entry(w)
+    btn_click = tk.Button(w, text='Click', command=process_fibonacci) # bind function
 
-w.mainloop() # 끌 때까지 계속 루프 돌음
+    # layout
+    lbl_display_fibonacci_recursion.pack()
+    en_input_number.pack(fill="x")
+    btn_click.pack(fill="x")
+
+    en_input_number.focus()
+    w.mainloop() # 끌 때까지 계속 루프 돌음
 
 
 # n = int(input("Input the number : ")) # Input box
